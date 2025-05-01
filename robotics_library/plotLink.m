825b2bbeeff8231d803b9e01464fe4e30713be43
tars <tm6898@gmail.com>
2025-05-02 00:41:55 +0200

    Modified lab_1

--- robotics_library/plotLink.m
+++ robotics_library/plotLink.m
@@ -1,7 +1,5 @@
-function plotLink(figureHandle, P_a, P_b)
+function plotLink(ax, P_a, P_b)
     % This function plots link between two points.
-
-    figure(figureHandle);
-    plot3([P_a(1)  P_b(1)], [P_a(2)  P_b(2)], [P_a(3)  P_b(3)], 'k-.','LineWidth', 4)
+    plot3(ax, [P_a(1)  P_b(1)], [P_a(2)  P_b(2)], [P_a(3)  P_b(3)], 'k-.','LineWidth', 4)
     axis equal;
 end
--- robotics_library/plotReferenceSystem.m
+++ robotics_library/plotReferenceSystem.m
@@ -1,10 +1,9 @@
-function plotReferenceSystem(figureHandle, P, x, y, z)
+function plotReferenceSystem(ax, P, x, y, z)
     % This function plots the reference system given the origin and the 
-    % axes vectors in the specified figure window.
+    % axes vectors wrt the origin frame.
 
-    figure(figureHandle);
-    quiver3(P(1), P(2), P(3), x(1)-P(1), x(2)-P(2), x(3)-P(3), 'r', 'LineWidth', 3)
-    quiver3(P(1), P(2), P(3), y(1)-P(1), y(2)-P(2), y(3)-P(3), 'g', 'LineWidth', 3)
-    quiver3(P(1), P(2), P(3), z(1)-P(1), z(2)-P(2), z(3)-P(3), 'b', 'LineWidth', 3)
-    axis equal;
+    quiver3(ax, P(1), P(2), P(3), x(1)-P(1), x(2)-P(2), x(3)-P(3), 'r', 'LineWidth', 3);
+    quiver3(ax, P(1), P(2), P(3), y(1)-P(1), y(2)-P(2), y(3)-P(3), 'g', 'LineWidth', 3);
+    quiver3(ax, P(1), P(2), P(3), z(1)-P(1), z(2)-P(2), z(3)-P(3), 'b', 'LineWidth', 3);
+    axis(ax, 'equal');
 end
