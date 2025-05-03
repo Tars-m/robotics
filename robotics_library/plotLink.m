62e34d045fb5957b0365bb12cf35f19d4d1b3940
tars <tm6898@gmail.com>
2025-05-03 21:36:07 +0200

    Modified lab_3 and lab_4

--- robotics_library/plotLink.m
+++ robotics_library/plotLink.m
@@ -1,12 +1,22 @@
-function plotLink(ax, P_a, P_b, d)
-    % Plots a link (line) between two 3D points and optionally labels translation
+function plotLink(ax, P_a, P_b, d, color)
+    % Plots a link (line) between two 3D points and optionally labels translation and sets color
 
-    plot3(ax, [P_a(1) P_b(1)], [P_a(2) P_b(2)], [P_a(3) P_b(3)], 'k', 'LineWidth', 3);
+    if nargin < 5
+        color = 'k';  % default color
+    end
+    if nargin < 4
+        d = '';       % no text label
+    end
 
-    if nargin > 3
+    % Plot the line
+    plot3(ax, [P_a(1) P_b(1)], [P_a(2) P_b(2)], [P_a(3) P_b(3)], ...
+          'Color', color, 'LineWidth', 3);
+
+    % Optionally plot text if d is not empty
+    if ~isempty(d)
         midPoint = (P_a + P_b) / 2;
-        text(ax, midPoint(1)+0.1, midPoint(2)+ 0.1, midPoint(3)+0.1, ...
-             d, ...
-             'FontSize', 15, 'Color', 'm', 'HorizontalAlignment', 'center');
+        offset = 0.1 * normalize(P_b - P_a); % offset along link direction
+        text(ax, midPoint(1) + offset(1), midPoint(2) + offset(2), midPoint(3) + offset(3), ...
+             d, 'FontSize', 15, 'Color', 'm', 'HorizontalAlignment', 'center');
     end
 end
--- robotics_library/transformation_using_DH.m
+++ robotics_library/transformation_using_DH.m
@@ -1,7 +1,10 @@
-function [DH, P] = transformation_using_DH(figureHandle, DH_0, P_0, a, alpha, d, phi)
+function [DH, P] = transformation_using_DH(ax, DH_0, P_0, a, alpha, d, phi, color)
     % This function calculates the DH transformation and plot the
     % corresponding reference system and link from a previous DH_0 and P_0
     
+    if nargin < 8
+        color = 'k';
+    end
     for i=1:length(a)
         DH_ = [...
             cos(phi(i)) -sin(phi(i))*cos(alpha(i)) sin(phi(i))*sin(alpha(i)) a(i)*cos(phi(i)); ...
@@ -10,8 +13,8 @@ function [DH, P] = transformation_using_DH(figureHandle, DH_0, P_0, a, alpha, d,
             0 0 0 1];
         
         [P, xyz] = transformReferenceSystem(DH_0 * DH_);
-        plotReferenceSystem(figureHandle, P, xyz(:,1), xyz(:,2), xyz(:,3));
-        plotLink(figureHandle, P, P_0);
+        plotReferenceSystem(ax, P, xyz(:,1), xyz(:,2), xyz(:,3));
+        plotLink(ax, P, P_0, '', color);
         DH_0 = DH_0*DH_;
         DH = DH_0;
         P_0 = P;
